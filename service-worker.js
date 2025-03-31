7616143d1c81647d5e8e9593e45780fbcc75d594
deisign <deisign@gmail.com>
2025-03-31 15:56:24 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const cacheName = 'coma-fm-cache-v1';
+const assets = [
+  './',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
